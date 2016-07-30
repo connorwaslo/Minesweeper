6d07bd27feeca1d88e31c3fb5aec37fa71bb8d1a
Connor Waslo <connorwaslo@gmail.com>
2016-07-29 20:53:53 -0700

    Update Input.java

--- src/minesweeper/Input.java
+++ src/minesweeper/Input.java
@@ -1,43 +1,19 @@
 package minesweeper;
 
+import org.lwjgl.glfw.GLFWMouseButtonCallback;
+
 import static org.lwjgl.glfw.GLFW.*;
 
 /**
  * Created by Connor Waslo on 7/27/2016.
  */
-public class Input {
+public class Input extends GLFWMouseButtonCallback {
 
-    private boolean pressed;
-    private int leftCount;
-    private int rightCount;
+    public static boolean[] currButtons = new boolean[2];
+    public static boolean[] prevButtons = new boolean[2];
 
-    public Input() {
-        pressed = false;
-        leftCount = 0;
-        rightCount = 0;
-    }
-
-    public void leftMousePressed(long window, Map map) {
-        if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS && leftCount < 1) {
-            leftCount++;
-
-            map.selectTile(map.getMouseRow(window), map.getMouseCol(window));
-        }
-
-        if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_RELEASE) {
-            leftCount = 0;
-        }
-    }
-
-    public void rightMousePressed(long window, Map map) {
-        if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS && rightCount < 1) {
-            rightCount++;
-
-            map.flagTile(map.getMouseRow(window), map.getMouseCol(window));
-        }
-
-        if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_RELEASE) {
-            rightCount = 0;
-        }
+    @Override
+    public void invoke(long window, int button, int action, int mods) {
+        currButtons[button] = action == GLFW_PRESS;
     }
 }
